85dfcb58d1debfc7644f9029d84cfd43aa6ce24f
5uori5 <69713809+5uori5@users.noreply.github.com>
2020-12-11 17:04:31 +0200

    Update temperature_parser.sh

--- temperature_parser.sh
+++ temperature_parser.sh
@@ -7,13 +7,13 @@ do
 #Subscribe to mqtt topic "esp" wait until get two messages(temperature and humidity)
 # and store collected data to "tempdata" variable. Quality of service for mqtt messages = 1,
 # so there should be always temperature data available when needed.
-tempdata=$(mosquitto_sub -q 1 -t esp/# -C 2)
+tempdata=$(mosquitto_sub '-q 1 -t esp/# -C 2')
 
 #throw data to database
-mysql -u root -p ryhma4test <<EOF
+mysql -u kalle -p testi <<EOF
 USE testi
-INSERT INTO saaData(temp, time, date)
-VALUES("$tempdata", "CURTIME", "CURDATE");
+INSERT INTO saaData(temp, hum, time, date)
+VALUES("$tempdata", "1", "CURTIME", "CURDATE");
 SELECT * FROM saaData
 EOF
 
